8e803837b50f5e361f8f04ba9c7db875a92fc828
Mengyu <john3345678@gmail.com>
2019-10-30 21:42:33 +0800

    Update resolvers.js and schema.graphql

--- resolvers.js
+++ resolvers.js
@@ -1,9 +1,20 @@
 const db = require('./db')
 const Query = {
+    // resolver function for welcome
     marvel_universe: () => {
        return 'Welcome to the Marvel Universe!' 
     },
-    characters:() => db.characters.list()
+    //resolver function for characters list
+    characters:() => db.characters.list(),
+    characterById:(root,args,context,info) => {
+        return db.characters.get(args.id);
+    }
 }
 
-module.exports = {Query}
+const Character = {
+    fullName:(root,args,context,info) => {
+        return root.firstName + ' ' + root.lastName
+    }
+}
+
+module.exports = {Query,Character}
